00121c5d9c8139047a50cc42ff10da01b0d89b10
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-07-01 15:19:28 -0400

    add create profile mutation

---
+++ src/helpers/createProfile.js
@@ -0,0 +1,19 @@
+import { useMutation, gql } from '@apollo/client';
+
+// Define mutation
+const createProfileMutation = gql`mutation CreateProfile($firstName: String!, $lastName: String!, $email: String!, $isVerified: Boolean!, $imageUrl: String!, $description: String!) {
+  createProfile(first_name: $firstName, last_name: $lastName, email: $email, is_verified: $isVerified, image_url: $imageUrl, description: $description) {
+    id
+    first_name
+    last_name
+    email
+    is_verified
+    image_url
+    description
+  }
+}`;
+
+function CreateProfile() {
+  // Pass mutation to useMutation
+  const [mutateFunction, { data, loading, error }] = useMutation(createProfileMutation);
+}
