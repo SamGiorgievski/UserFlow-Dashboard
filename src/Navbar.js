7bab12cda23d7f3c166cb15a9a5e987efb75b50c
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-07-01 16:31:07 -0400

    add navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,24 @@
 import './Navbar.css';
-import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Button, Switch } from '@mui/material';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+
 
 import React from 'react'
 
 export default function Navbar() {
+
+  const label = { inputProps: { 'aria-label': 'Switch demo' } };
+  
   return (
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Viralnation
           </Typography>
+          <LightModeIcon />
+          <Switch {...label} color="primary" />
+          <DarkModeIcon />
         </Toolbar>
       </AppBar>
   );
