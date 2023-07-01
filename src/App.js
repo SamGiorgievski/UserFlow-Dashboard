7cb9d1e81824561ae1528029ef853033b5327939
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-07-01 14:30:56 -0400

    added get_profiles query

--- src/App.js
+++ src/App.js
@@ -3,31 +3,38 @@ import { useQuery, gql } from '@apollo/client';
 
 function App() {
 
-  const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
+  const GET_PROFILES = gql`
+  query GetAllProfiles($orderBy: globalOrderBy, $searchString: String, $rows: Int, $page: Int) {
+    getAllProfiles(orderBy: $orderBy, searchString: $searchString, rows: $rows, page: $page) {
+      size
+      profiles {
+        id
+        first_name
+        last_name
+        email
+        is_verified
+        image_url
+        description
+      }
     }
   }
   `;  
 
   function DisplayLocations() {
-    const { loading, error, data } = useQuery(GET_LOCATIONS);
-    // console.log(useQuery(GET_LOCATIONS));
+    const { loading, error, data } = useQuery(GET_PROFILES);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error : {error.message}</p>;
 
-    return data.locations.map(({ id, name, description, photo }) => (
+    console.log(data.getAllProfiles.profiles);
+
+    return data.getAllProfiles.profiles.map(({ description, email, first_name, id, image_url, is_verified, last_name, __typename
+    }) => (
       <div key={id}>
-        <h3>{name}</h3>
-        <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+        <h3>{first_name}</h3>
+        {/* <img width="400" height="250" alt="location-reference" src={`${photo}`} /> */}
         <br />
-        <b>About this location:</b>
-        <p>{description}</p>
+        <p>{email}</p>
         <br />
       </div>
     ));
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, ApolloLink } from '@apollo/client';
+
+
+const httpLink = new HttpLink({
+  uri: 'https://api.poc.graphql.dev.vnplatform.com/graphql'
+});
+
 
 const client = new ApolloClient({
-  uri: 'https://flyby-router-demo.herokuapp.com/',
+  uri: 'https://api.poc.graphql.dev.vnplatform.com/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjYW5kaWRhdGVfbmFtZSI6IlNhbXVlbCIsImlzX2NhbmRpZGF0ZSI6dHJ1ZSwiaWF0IjoxNjg3OTc3MTA0LCJleHAiOjE2ODg0OTU1MDR9.Bfro7iGtbJXFIReb9x4dlnApEKEcOafN_Jj68VC2j7U'
+  }
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
