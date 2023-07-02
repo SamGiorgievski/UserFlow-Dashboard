b48dc86bc7144ee7330faa27754d2aa38b072681
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-07-02 15:57:48 -0400

    modal styling

--- src/Profilemodal.js
+++ src/Profilemodal.js
@@ -1,7 +1,24 @@
 import React from 'react'
-import { Box, Typography, Modal } from '@mui/material';
+import { Box, Typography, Modal, TextField, Switch, Grid } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Profilemodal( { handleOpen, handleClose, open}) {
+
+  const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    height: 600,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
   return (
     <Modal
       open={open}
@@ -9,13 +26,87 @@ export default function Profilemodal( { handleOpen, handleClose, open}) {
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
       >
-      <Box>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Text in a modal
+      <Box sx={style}>
+      
+        <Typography 
+        id="modal-modal-title" 
+        variant="h6" 
+        component="h2" 
+        style={{ 
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginBottom: '30px'
+        }}>
+          Create Profile
+          <CloseIcon onClick={handleClose}/>
         </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-        </Typography>
+
+        
+
+        <Grid container spacing={2} rowSpacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="outlined-required"
+            label="Required"
+            defaultValue="Image link"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="First Name"
+              fullWidth
+            />
+        </Grid>
+        <Grid item xs={6}>
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="Last Name"
+              fullWidth
+            />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="outlined-required"
+            label="Required"
+            defaultValue="Email"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="outlined-required"
+            label="Required"
+            defaultValue="Description"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12} >
+          <Box sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+        }} >
+            <TextField
+              disabled
+              id="outlined-disabled"
+              label="Disabled"
+              defaultValue="Hello World"
+              fullWidth
+            /> 
+            <Switch {...label} defaultChecked />
+          </Box>
+        </Grid>
+      </Grid>
+
+        
       </Box>
     </Modal>
   )
